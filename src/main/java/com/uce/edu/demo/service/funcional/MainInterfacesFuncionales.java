77c62f845f6bc507eb64b1687eedfb123ce02eaa
Usuario <Usuario@DESKTOP-CP8F959>
2022-08-23 20:23:00 -0500

    taller34

--- src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
+++ src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
@@ -6,6 +6,10 @@ public class MainInterfacesFuncionales {
 	private static Logger LOG = LogManager.getLogger(MainInterfacesFuncionales.class.getName());
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ConsumoMetodosHighOrder metodosHo=new ConsumoMetodosHighOrder();
+		
+		
 		//SUPPLIER
 		// Clases
 		
@@ -20,6 +24,15 @@ public class MainInterfacesFuncionales {
 		
 		IPersonaSupplier<String>supplierTE2=()->"Antony TE 2";
 		LOG.info("Supplier ClaseTE: "+supplierTE2.getNombre());
+		//Metodos HO
+		String valor=metodosHo.consumirSuplier(()->"Hola Mundo");
+		LOG.info("HO supplier: "+valor);
+		
+		String valorHo=metodosHo.consumirSuplier(()->{
+			String val="";
+			return val;
+		});
+		LOG.info("HO supplier: "+valor);
 		// CONSUMER
 		// Clases
 		IPersonaConsumer<String>consumerPersona=new PersonaConsumerImpl();
@@ -28,19 +41,41 @@ public class MainInterfacesFuncionales {
 		IPersonaConsumer<String>consumerLambda=cadena->System.out.println("cadena");
 		consumerLambda.accept("Antony Consumer Lambda");
 		// Lambdas
-
+//Metodos HigORder
+		metodosHo.consumirConsumer(cadena->System.out.println(cadena), 2);
+		
 		// PREDICATE
 		// Clases
 		// Lambdas
-
+		IPersonaPredicate<String>predicateLambda=cadena->cadena.contains("z");
+		LOG.info("Predicate Lamda: "+predicateLambda.evaluar("Manxz"));
+//MetodosHO
+		boolean respuesta=metodosHo.consumirPredicate(cadena->cadena.contains("z"), "zzebasz");
+		LOG.info("HOpredicate "+respuesta);
+		
 		// FUNCTION
 		// Clases
 		// Lambdas
-
+		IPersonaFunction<Integer, String>funcLambda=cadena->{
+			Integer aval=Integer.parseInt(cadena);
+			Integer valorFinal=aval-2;
+			return valorFinal;
+		};
+		LOG.info("Function lambda; "+funcLambda.aplicar("9"));
+//HOMetodos
+		String avalFn=metodosHo.consumirFunction(aval->{
+			String rer=aval.toString()+"A";
+			return rer;
+		}, 1);
+		LOG.info("HO function "+avalFn);
 		// UNARY OPERATOR (FUNCTION
 		// Clases
 		// Lambdas
-
+IPersonaFunctionUnaryOperator<String> unaryLambda=cadena->{
+	String valorFinal=cadena.concat("sufijo");
+	return valorFinal;
+};
+LOG.info("Predicate Lambdas: "+unaryLambda.aplicar("Antony"));
 	}
 
 }
